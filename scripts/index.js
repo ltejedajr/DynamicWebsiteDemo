c3026cacb4f90aedfa0ffc1714d80a9e6e0627e8
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-11-08 13:48:21 -0500

    Added initial structure

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+windows.onload = intit;
+
+function init() {
+    const showMeTheJokesBtn = document.getElementById("showMeTheJokesBtn");
+    showMeTheJokesBtn.onclick = showMeTheJokesBtnClicked;
+
+    popularCategoriesDropdown();
+
+}
+
+function popularCategoriesDropdown() {
+// populate categories with distinct categories from the jokes array
+
+    
+
+
+
+}
+
+function showMeTheJokesBtnClicked() {
+// iddentify the selected catergory and loop through and show those jokes
+
+}
